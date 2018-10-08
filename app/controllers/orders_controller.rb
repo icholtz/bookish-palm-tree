91f0cd488addf0c9a98709947cb2ceb1bf58043c
Icholtz <hatt23@centrum.cz>
2018-10-08 08:17:23 +0100

    modifying carousel

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,11 @@
 class OrdersController < ApplicationController #The line declares a class that inherits from ApplicationController, meaning it has access to all the methods inside your application_controller.rb file
   def index
+    # @orders = Order.all
+    @orders = Order.includes(:umbrella).all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
