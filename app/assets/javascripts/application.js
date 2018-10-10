f8d27eb0fa11074f871c870bdd8854695712e693
Icholtz <hatt23@centrum.cz>
2018-10-10 17:25:07 +0100

    figuring out products images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require jquery3
+//= require bootstrap-sprockets
+//= require popper
+//= require bootstrap
 //= require_tree .
